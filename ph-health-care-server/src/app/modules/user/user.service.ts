1dcb30da23839c5718321b320a07b3eb1af955bb
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-11-20 19:28:44 +0600

    update schema

---
+++ ph-health-care-server/src/app/modules/user/user.service.ts
@@ -0,0 +1,30 @@
+import { prisma } from "../../shared/prisma";
+import { createPatientInput } from "./user.interface";
+import bcrypt from "bcryptjs"
+
+const createPatient = async(payload: createPatientInput) => {
+    const hashPassword = await bcrypt.hash(payload.password, Number(process.env.BCRYPT_SALT));
+
+    const result = await prisma.$transaction(async (tnx) => {
+        await tnx.user.create({
+            data: {
+                email: payload.email,
+                password: hashPassword
+            }
+        });
+
+        return await tnx.patient.create({
+            data: {
+                name: payload.name,
+                email: payload.email
+            }
+        })
+    });
+
+    return result
+
+}
+
+export const UserService = {
+    createPatient
+}
